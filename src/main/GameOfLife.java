a5c0f1ed25544f2a2a855b7fd44ac4c60dbd603d
Raj Bharath <rbrajbharath1@gmail.com>
2015-03-01 16:31:34 +0530

    Added - GameOfLife class
    this class provides the interface for starting the game with seed and generation count

---
+++ src/main/GameOfLife.java
@@ -0,0 +1,18 @@
+package main;
+
+public class GameOfLife {
+    private Grid grid;
+    private int generationCount;
+
+    public void start(int[][] seed, int generationCount) {
+        grid = new GridBuilder().build(seed);
+        this.generationCount = generationCount;
+        evolve();
+    }
+
+    private void evolve() {
+        for (int i = 0; i < generationCount; i++) {
+            grid.forwardToNextGeneration();
+        }
+    }
+}
